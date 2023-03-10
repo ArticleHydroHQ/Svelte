4b9cf32507ac128a8a493b92f745b0e61e99d8fb
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-10 18:35:48 +0100

    cdn is fine I think

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1 >Welcome to the main page of Article Hydro HQ </h1>
+<h1 class="text-3xl font-bold underline">Welcome to the main page of Article Hydro HQ </h1>
 <p>this website is all about Minecraft servers and useful support articles written by people who understand what they write about. </p>
