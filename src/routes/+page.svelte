526684f98d034805d84b595e2b48f542073f08c0
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-10 18:26:46 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 >Welcome to the main page of Article Hydro HQ </h1>
+<p>this website is all about Minecraft servers and useful support articles written by people who understand what they write about. </p>
