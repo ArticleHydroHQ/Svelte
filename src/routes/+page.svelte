5d14fe9dcb6acedff43f03af75ca2ae871098872
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-10 18:56:15 +0100

    how?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1 class="text-3xl font-bold underline">Welcome to the main page of Article Hydro HQ </h1>
+<h1 class="text-3xl text-center hover:text-4xl">Welcome to the main page of Article Hydro HQ </h1>
 <p>this website is all about Minecraft servers and useful support articles written by people who understand what they write about. </p>
+<p><a href="articels/cracked.svelte">link</a></p>
