b833c005562acb1d0037ae0c658968f69099c160
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-11 12:15:58 +0100

    that is how it works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <h1 class="text-3xl text-center hover:text-4xl">Welcome to the main page of Article Hydro HQ </h1>
 <p>this website is all about Minecraft servers and useful support articles written by people who understand what they write about. </p>
-<p><a href="articels/cracked">link</a></p>
+<p><a href="articels/">link</a></p>
