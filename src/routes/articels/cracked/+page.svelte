6e0cc6e304609f1ffb3586c07a58a5a5a643cc4b
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-11 14:45:25 +0100

    update

--- src/routes/articels/cracked/+page.svelte
+++ src/routes/articels/cracked/+page.svelte
@@ -1,1 +1,8 @@
-<p>test</p> <h1>asdfassdf</h1>
+<script>
+    let article = 'cracked'
+</script>
+<h1 class="text-center text-4xl">{article}</h1>
+<p>Cracked, or officially known as online-mode in minecraft server.properties is a setting that allows players to join a server without being checked against the Mojang authentication servers. This can also be used to allow cracked players to join the game.<br><br> </p>
+<p class="font-bold text-xl">There is no premium minecraft. This would imply the existence of a free version, which Mojang does not offer. ALWAYS buy the game and do not use illegal, pirated launchers such as TLauncher.<br><br></p>
+<p class="text-center text-2xl">Risks of using cracked</p>
+<p>Cracked is deeply insecure and allows for many exploits. It’s important to note all of these before deciding to open your server to these risks without the necessary measures. <i><b>Again, we advise against turning cracked on for your server.<b><i></p>
