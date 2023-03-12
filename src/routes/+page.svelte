c19cb26c150b8a6e7c18f18ed193c1bba16aeddc
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-12 15:59:07 +0100

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,20 @@
-<div class="grid grit-rows-4 grid-flow-col gap-4">
+<div class="grid grid-cols-4 gap-4">
         <div>
         <span class="text-zinc-50 text-xl ">
             <i class="fa-brands fa-github fa-xl "></i>
         </span>
     </div>
+    <div class="text">
+
+    </div>
     <div>
-        <!--needs to be emty-->
-    
+
     </div>
     <div class="text-right">
         <span class="text-zinc-50 text-xl">
             <a href="https://discord.gg/zUu492YQk3">
                 <i class="fa-brands fa-discord fa-xl ">Discord</i>
             </a>
-        </span>
     </div>
 </div>
 
