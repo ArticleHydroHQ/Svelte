940ace1cfe8450379dcf9438569d5e2598fd1d50
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-12 16:11:52 +0100

    bigins to look like something

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,35 @@
-<div class="grid grid-cols-4 gap-4">
+<div class="grid grid-cols-2 gap-4">
         <div>
         <span class="text-zinc-50 text-xl ">
             <i class="fa-brands fa-github fa-xl "></i>
         </span>
-    </div>
-    <div class="text">
-
-    </div>
-    <div>
-
     </div>
     <div class="text-right">
         <span class="text-zinc-50 text-xl">
             <a href="https://discord.gg/zUu492YQk3">
                 <i class="fa-brands fa-discord fa-xl ">Discord</i>
             </a>
+    </div>
+</div>
+<div class="grid grid-cols-3 gap-4">
+    <div>
+        <!--empy-->
+    </div>
+    <div>
+        <p> logo here </p>
+    </div>
+    <div>
+        <!--empy-->
+    </div>
+</div>
+<div class="grid grid-cols-3 gap-4">
+    <div>
+
+    </div>
+    <div>
+        <h1 class="text-3xl">
+            Article Hydro HQ
+        </h1>
     </div>
 </div>
 
