7bcbc387270c9611f5bcfcb635c48a2cc653232e
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-12 16:58:47 +0100

    why?

---
+++ src/routes/aboutus/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import aboutUsBlocks from 'card.svelte';
+  
+  const aboutUsBlocks = [
+    {
+      name: 'John Doe',
+      functionText: 'Founder and CEO',
+      introduction: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    },
+    {
+      name: 'Jane Smith',
+      functionText: 'Marketing Director',
+      introduction: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+  ];
+</script>
+
+<aboutUsBlocks {aboutUsBlocks} />
+
+  
---
+++ src/routes/aboutus/card.svelte
@@ -0,0 +1,18 @@
+<script>
+    /**
+   * @type {any}
+   */
+     export let aboutUsBlocks;
+  </script>
+  
+  {#each aboutUsBlocks as block}
+    <div class="about-us-block">
+      <img class="profile-picture" src="https://example.com/profile-pic.png" alt="Profile Picture">
+      <div class="info-block">
+        <h2 class="name">{block.name}</h2>
+        <p class="function">{block.functionText}</p>
+        <p class="introduction">{block.introduction}</p>
+      </div>
+    </div>
+  {/each}
+  
