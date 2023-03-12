84d1e6e3ae0baa7cf7452014700f1f955bbaffbc
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-12 15:40:28 +0100

    upperrow done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,17 @@
         <span class="text-zinc-50 text-xl ">
             <i class="fa-brands fa-github fa-xl "></i>
         </span>
-        <p>github</p>
     </div>
     <div>
         <!--needs to be emty-->
+    
     </div>
     <div class="text-right">
-        <p><a href="https://discord.gg/zUu492YQk3">discord</a></p>
+        <span class="text-zinc-50 text-xl">
+            <a href="https://discord.gg/zUu492YQk3">
+                <i class="fa-brands fa-discord fa-xl ">Discord</i>
+            </a>
+        </span>
     </div>
 </div>
 
