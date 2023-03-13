9c035c4d3678631ae4e300909d9aae50c7343493
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-13 20:53:36 +0100

    test

---
+++ src/routes/aboutus/+page.svelte
@@ -0,0 +1,19 @@
+
+<script>
+    import { members } from './members.js';
+  </script>
+  
+  <h1 class="text-3xl font-bold mb-6">About Us</h1>
+  
+  <div class="flex flex-wrap -mx-4">
+    {#each members as member}
+    <div class="rounded-md border-gray-50">
+        <div >
+            <img class="rounded-full" src="{member.photo} alt={member.name}">
+        </div>
+        <div>
+            <p> {member.name}</p>
+        </div>
+    </div>
+    {/each}
+  </div>
---
+++ src/routes/aboutus/members.js
@@ -0,0 +1,22 @@
+// members.js
+export const members = [
+    {
+      name: 'John Smith',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae tellus enim.',
+      function: 'CEO',
+      photo: 'http://1000logos.net/wp-content/uploads/2017/05/New-YouTube-logo.jpg'
+    },
+    {
+      name: 'Jane Doe',
+      description: 'Nulla eget velit et sem pulvinar mollis. Phasellus eu felis id diam blandit aliquet.',
+      function: 'COO',
+      photo: 'https://picsum.photos/id/1011/200/200'
+    },
+    {
+      name: 'Bob Johnson',
+      description: 'Nam tincidunt nulla eu mauris faucibus feugiat. Sed fringilla nisl eu nisi efficitur.',
+      function: 'CTO',
+      photo: 'https://picsum.photos/id/1015/200/200'
+    }
+  ];
+  
