e01b2239f00d93b283b3387df73aa0ac06415150
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-13 21:05:01 +0100

    updated to get it right

--- src/routes/aboutus/members.js
+++ src/routes/aboutus/members.js
@@ -1,22 +1,22 @@
 // members.js
 export const members = [
     {
-      name: 'John Smith',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae tellus enim.',
-      function: 'CEO',
-      photo: 'http://1000logos.net/wp-content/uploads/2017/05/New-YouTube-logo.jpg'
+      name: 'Dobbie',
+      description: 'I am Dobbie, founder of AHHQ progamer, article writer',
+      function: 'founder',
+      photo: 'https://cdn.discordapp.com/avatars/820255805257023498/74d5106b0e3be86c8abcd3e6dbbfd34e.webp?size=4096'
     },
     {
-      name: 'Jane Doe',
-      description: 'Nulla eget velit et sem pulvinar mollis. Phasellus eu felis id diam blandit aliquet.',
-      function: 'COO',
-      photo: 'https://picsum.photos/id/1011/200/200'
+      name: 'Josh',
+      description: "Hey, I'm Josh. Software Developer&owner @ Blue Atomic, and developer /article writer at AHHQ.",
+      function: 'Article writer and Devolper',
+      photo: 'https://cdn.discordapp.com/avatars/839237573595365406/b1107ca4a66f957426ea20aed69a7249.webp?size=4096'
     },
     {
-      name: 'Bob Johnson',
+      name: 'Soapy7261',
       description: 'Nam tincidunt nulla eu mauris faucibus feugiat. Sed fringilla nisl eu nisi efficitur.',
-      function: 'CTO',
-      photo: 'https://picsum.photos/id/1015/200/200'
+      function: 'Article Writer and Devolper',
+      photo: 'https://cdn.discordapp.com/avatars/513072262409355274/a_7a69967b586851f3ee7ce53bf80835ac.gif?size=4096'
     }
   ];
   
