6a909d8096069a8d61db7d9c6b981d656cf83133
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-13 18:38:19 +0100

    WORK!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,66 +1,51 @@
 <script>
     import Icon from "$lib/assets/logo.png"
  </script>
-
-<div class="grid grid-cols-2 gap-4">
+<div class="w-screen h-screen">
+    <div class="grid grid-cols-2 gap-4">
+            <div>
+            <span class="text-zinc-50 text-xl ">
+                <i class="fa-brands fa-github fa-xl "></i>
+            </span>
+        </div>
+        <div class="text-right">
+            <span class="text-zinc-50 text-xl">
+                <a href="https://discord.gg/zUu492YQk3">
+                    <i class="fa-brands fa-discord fa-xl ">Discord</i>
+                </a>
+        </div>
+    </div>
+    <div class="grid grid-cols-3 gap-4">
         <div>
-        <span class="text-zinc-50 text-xl ">
-            <i class="fa-brands fa-github fa-xl "></i>
-        </span>
+            <!--empy-->
+        </div>
+        <div>
+            <img src={Icon} alt="logo"/>
+        </div>
+        <div>
+            <!--empy-->
+        </div>
     </div>
-    <div class="text-right">
-        <span class="text-zinc-50 text-xl">
-            <a href="https://discord.gg/zUu492YQk3">
-                <i class="fa-brands fa-discord fa-xl ">Discord</i>
-            </a>
+    <div class="grid grid-cols-3 gap-4">
+        <div>
+        </div>
+        <div>
+            <h1 class="text-2xl text-center">
+            Minecraft server info for you
+            </h1>
+        </div>
+        <div></div>
+    </div>
+    <div class="grid grid-cols-5 gap-4 text-center">
+        <div></div>
+        <div>
+            <p>bt-1</p>
+        </div>
+        <div></div>
+        <div>
+            <p>bt-1</p>
+        </div>
+        <div></div>
+
     </div>
 </div>
-<div class="grid grid-cols-3 gap-4">
-    <div>
-        <!--empy-->
-    </div>
-    <div>
-        <img src={Icon} alt="logo"/>
-    </div>
-    <div>
-        <!--empy-->
-    </div>
-</div>
-<div class="grid grid-cols-3 gap-4">
-    <div>
-    </div>
-    <div>
-        <h1 class="text-2xl text-center">
-         Minecraft server info for you
-        </h1>
-    </div>
-    <div></div>
-</div>
-<div class="grid grid-cols-5 gap-4 text-center">
-    <div></div>
-    <div>
-        <p>bt-1</p>
-    </div>
-    <div></div>
-    <div>
-        <p>bt-1</p>
-    </div>
-    <div></div>
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
