97020745ced08eb8a1595ac9ee682ac8094b9b08
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-14 20:22:07 +0100

    added color support

--- src/routes/aboutus/+page.svelte
+++ src/routes/aboutus/+page.svelte
@@ -14,13 +14,15 @@
   </div>-->
 <div class="flex gap-8 basis-64 flex-wrap ">
 {#each members as member }
-  <div class=" rounded-md border-4 text-center flex flex-col justify-center items-center max-w-xs">
-    <div >
+  <div class=" rounded-md border-4 {member.color} text-center flex flex-col justify-center items-center max-w-xs">
+    <div class="pd-36">
       <img class="rounded-full border-4 content-center " src={member.photo} alt={member.name}>
     </div>
-    <p class="text-5xl ">{member.name}</p>
-    <p class="text-3xl"> {member.function}</p>
-    <p class="">{member.description}</p>
+    <div>
+      <p class="text-5xl ">{member.name}</p>
+      <p class="text-3xl"> {member.function}</p>
+      <p class="">{member.description}</p>
+    </div>
   </div>
 
 {/each}
