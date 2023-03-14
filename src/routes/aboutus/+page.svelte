c233cff206172c1a01fc2c0ec5f6a52acf91ae8e
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-14 18:12:12 +0100

    starting!

--- src/routes/aboutus/+page.svelte
+++ src/routes/aboutus/+page.svelte
@@ -4,11 +4,19 @@
   
   <h1 class="text-3xl font-bold mb-6">About Us</h1>
   
-  <div class="flex flex-wrap -mx-4">
+<!--  <div class="flex flex-wrap -mx-4">
     {#each members as member}
     <div class="rounded-md border-gray-500">
 
     <p>{member.function}</p>
     </div>
     {/each}
+  </div>-->
+<div class="flex row  flex-wrap ">
+{#each members as member }
+  <div class=" rounded-md border-4">
+    <img class="rounded-full" src={member.photo} alt={member.name}>    
   </div>
+
+{/each}
+</div>
