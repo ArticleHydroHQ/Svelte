2db95cc25d81b63f2d901dc46b531a0c2f85c1f3
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-14 20:00:13 +0100

    works!

--- src/routes/aboutus/+page.svelte
+++ src/routes/aboutus/+page.svelte
@@ -14,14 +14,14 @@
   </div>-->
 <div class="flex gap-8 basis-64  ">
 {#each members as member }
-  <div class=" rounded-md border-4 text-center ">
+  <div class=" rounded-md border-4 text-center flex flex-col justify-center items-center">
 
     <div>
-      <img class="rounded-full border-4 place-self-center" src={member.photo} alt={member.name}>
+      <img class="rounded-full border-4 content-center" src={member.photo} alt={member.name}>
     </div>
-    <p class="text-center text-5xl">{member.name}</p>
-    <p class="text-center text-3xl"> {member.function}</p>
-    <p class="text-center ">{member.description}</p>
+    <p class="text-5xl">{member.name}</p>
+    <p class="text-3xl"> {member.function}</p>
+    <p class="text-left">{member.description}</p>
   </div>
 
 {/each}
