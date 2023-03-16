aadd038aacb77057623f8c30ba443e5408cbd2d4
Dobby-progam <87517487+Dobby-progam@users.noreply.github.com>
2023-03-16 18:17:37 +0100

    Remove Fixes #2

--- src/routes/aboutus/+page.svelte
+++ src/routes/aboutus/+page.svelte
@@ -4,14 +4,7 @@
   
   <h1 class="text-3xl font-bold mb-6">About Us</h1>
   
-<!--  <div class="flex flex-wrap -mx-4">
-    {#each members as member}
-    <div class="rounded-md border-gray-500">
 
-    <p>{member.function}</p>
-    </div>
-    {/each}
-  </div>-->
 <div class="flex gap-8 basis-64 flex-wrap ">
 {#each members as member }
   <div class=" rounded-md border-4 {member.color} text-center flex flex-col justify-center items-center max-w-xs">
--- src/routes/articels/cracked/+page.svelte
+++ src/routes/articels/cracked/+page.svelte
@@ -1,13 +1,15 @@
 <script>
-    let article = 'cracked'
-</script>
-<h1 class="text-center text-4xl">{article}</h1>
+import { info } from './info.js';
+import Navbar from '../../navbar.svelte';
+  </script>
+  <Navbar></Navbar>
+<h1 class="text-center text-4xl">{info[0].name}</h1>
 <p>Cracked, or officially known as online-mode in minecraft server.properties is a setting that allows players to join a server without being checked against the Mojang authentication servers. This can also be used to allow cracked players to join the game.<br><br> </p>
 <p class="font-bold text-xl">There is no premium minecraft. This would imply the existence of a free version, which Mojang does not offer. ALWAYS buy the game and do not use illegal, pirated launchers such as TLauncher.<br><br></p>
 <p class="text-center text-3xl">Risks of using cracked</p>
-<p>Cracked is deeply insecure and allows for many exploits. It’s important to note all of these before deciding to open your server to these risks without the necessary measures. <i><b>Again, we advise against turning cracked on for your server.<b><i><br><br></p>
+<p>Cracked is deeply insecure and allows for many exploits. It's important to note all of these before deciding to open your server to these risks without the necessary measures. <i><b>Again, we advise against turning cracked on for your server.<b><i><br><br></p>
 <p class="text-center text-2xl">Username exploiting</p>
-<p>When using cracked, accounts are not authenticated by Mojang’s servers. This essentially means that acccount data is solely tied to usernames. This allows for people to log in with any username, thus receiving all the data tied to that username. Bought accounts can also be affected by this. <i> This can be prevented by installing authentication plugins like LoginSecurity. <i><br><br></p>
+<p>When using cracked, accounts are not authenticated by Mojang's servers. This essentially means that acccount data is solely tied to usernames. This allows for people to log in with any username, thus receiving all the data tied to that username. Bought accounts can also be affected by this. <i> This can be prevented by installing authentication plugins like LoginSecurity. <i><br><br></p>
 <p class="text-center text-2xl">Bot Raids</p>
 <p> Not to be confused with DdoS-attacks, bot raids are a serious issue on cracked servers lately. A bot raid causes many bots to join your server, possibly overloading it and filling up storage with unnecessary playerdata. You cannot prevent this in cracked servers, just disable cracked and buy the game.<br><br></p>
 <p class="text-center text-2xl font-extrabold">But i have no money!111!1!!</p>
